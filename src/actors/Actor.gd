21dce797e625ac1d410ce6814364a5f1a29d5fb2
xenoscience <xmdscience@gmail.com>
2022-10-19 21:28:10 -0600

    Added jumping to player with some minor refactors

--- src/actors/Actor.gd
+++ src/actors/Actor.gd
@@ -3,14 +3,7 @@ class_name Actor
 
 # Gravity
 export var speed := Vector2(1000.0, 1000.0)
-export var gravity := 30.0
-var velocity :=  Vector2.ZERO
+export var gravity := 1000.0
+var _velocity :=  Vector2.ZERO
 
-func _physics_process(delta : float) -> void:
-		velocity.y += gravity * delta
-		
-		# Max Move Speed
-		#velocity.y = max(velocity.y, speed.y)
-		#velocity.x = max(velocity.x, speed.x)
-		
-		
+var FLOOR_NORMAL := Vector2.UP
