db4a2b2a9f433b78b2d23fb85cf7979c80eca134
JollyWolly <76886052+JollyWolly@users.noreply.github.com>
2022-10-20 22:26:26 -0600

    glide values adjusted

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	
 	if Input.is_action_pressed("jump") and -_velocity.y < 0:
-		_velocity.y *= 0.5
+		_velocity.y *= 0.825
 		
 	_velocity =  move_and_slide(_velocity, FLOOR_NORMAL)
 	
